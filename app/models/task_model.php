cdb8ef2958c64beeeeb0ab2afa61072d3c7795f9
luikz <luiz.carlos.techie@gmail.com>
2020-12-02 10:58:44 -0300

    get tasks

--- app/models/task_model.php
+++ app/models/task_model.php
@@ -1,8 +1,58 @@
 <?php 
 class Task_model extends CI_Model {
     public function get_task($id) {
+        $this->db->select('
+                tasks.task_name,
+                tasks.id,
+                tasks.create_date,
+                tasks.task_body,
+                tasks.is_complete,
+                lists.id as list_id,
+                lists.list_name,
+                lists.list_body
+        ');
+        $this->db->from('tasks');
+        $this->db->join('lists', 'lists.id = tasks.list_id');
+        $this->db->where('tasks.id',$id);
+        $query = $this->db->get();
+        if($query->num_rows() != 1) {
+            return FALSE;
+        }else {
+            return $query->row();
+        }
+    }
+
+    public function get_task_list_id($task_id) {
+        $this->db->where('id',$task_id);
         $query = $this->db->get('tasks');
-        $this->db->where('id', $id);
+        return $query->row()->list_id;
+    }
+    public function get_list_name($list_id) {
+        $this->db->where('id',$list_id);
+        $query = $this->db->get('lists');
+        return $query->row()->list_name;
+    }
+    
+    public function create_task($data) {
+        $insert = $this->db->insert('tasks',$data);
+        return $insert;
+    }
+    
+    public function edit_task($task_id, $data) {
+        $this->db->where('id', $task_id);
+        $this->db->update('tasks', $data);
+        return TRUE;
+    }
+
+    public function delete($task_id) {
+        $this->db->where('id',$task_id);
+        $this->db->delete('tasks');
+        return;
+    }
+    
+    public function get_task_data($task_id) {
+        $this->db->where('id',$task_id);
+        $query = $this->db->get('tasks');
         return $query->row();
     }
 
