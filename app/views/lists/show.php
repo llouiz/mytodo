26239ebf7939343b023a6a790450815fe3ae1b8a
luikz <luiz.carlos.techie@gmail.com>
2020-12-03 10:09:00 -0300

    messages and task status

--- app/views/lists/show.php
+++ app/views/lists/show.php
@@ -5,7 +5,45 @@
     <li><a onclick="return confirm('Are you sure?')" href="<?php echo base_url(); ?>lists/delete/<?php echo $list->id; ?>">Delete List</a></li>
 </ul>
 <h1><?php echo $list->list_name; ?></h1>
+<?php if($this->session->flashdata('task_deleted')) : ?>
+    <?php echo '<p class="text-success">' .$this->session->flashdata('task_deleted') . '</p>'; ?>
+<?php endif; ?>
+<?php if($this->session->flashdata('task_created')) : ?>
+    <?php echo '<p class="text-success">' .$this->session->flashdata('task_created') . '</p>'; ?>
+<?php endif; ?>
+<?php if($this->session->flashdata('task_updated')) : ?>
+    <?php echo '<p class="text-success">' .$this->session->flashdata('task_updated') . '</p>'; ?>
+<?php endif; ?>
+<?php if($this->session->flashdata('marked_complete')) : ?>
+    <?php echo '<p class="text-success">' .$this->session->flashdata('marked_complete') . '</p>'; ?>
+<?php endif; ?>
+<?php if($this->session->flashdata('marked_new')) : ?>
+    <?php echo '<p class="text-success">' .$this->session->flashdata('marked_new') . '</p>'; ?>
+<?php endif; ?>
 Created on: <Strong><?php echo date("n-j-Y",strtotime($list->create_date)); ?></strong>
 <br/><br/>
 <div style="max-width:500px;"><?php echo $list->list_body; ?></div>
 <br/><br/>
+<h4>Active Tasks</h4>
+<?php if($active_tasks) : ?>
+    <ul>
+        <?php foreach($active_tasks as $task) : ?>
+            <li><a href="<?php echo base_url(); ?>tasks/show/<?php echo $task->task_id; ?>"><?php echo $task->task_name; ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+<?php else : ?>
+    <p>There are no active tasks</p>
+<?php endif; ?>
+<br/>
+
+<h4>Completed Tasks</h4>
+<?php if($inactive_tasks) : ?>
+    <ul>
+        <?php foreach($inactive_tasks as $task) : ?>
+            <li><a href="<?php echo base_url(); ?>tasks/show/<?php echo $task->task_id; ?>"><?php echo $task->task_name; ?></a></li>
+        <?php endforeach; ?>
+    </ul>
+<?php else : ?>
+    <p>There are no completed tasks</p>
+<?php endif; ?>
+<br/>
