9308baaaf2b46b1884444d716c142da146e6e0f9
luikz <luiz.carlos.techie@gmail.com>
2020-12-04 11:53:07 -0300

    get users tasks

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -6,7 +6,7 @@ class Home extends CI_Controller{
 			$user_id = $this->session->userdata('user_id');
 			//Get all lists from the model
 			$data['lists'] = $this->List_model->get_all_lists($user_id);
-			//$data['tasks'] = $this->Task_model->get_users_tasks($user_id);
+			$data['tasks'] = $this->Task_model->get_users_tasks($user_id);
 		}
 
 		$data['main_content'] = 'home';
